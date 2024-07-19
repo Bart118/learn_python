28d6698ae141159821d338f2c964be1439c86a97
Bart118 <sbart8291@gmail.com>
2024-07-19 14:18:56 -0500

    added guess limit

--- guess_my_number.py
+++ guess_my_number.py
@@ -4,6 +4,8 @@
 #The player tries to guess it and the computer lets
 #the player know if the guess is too high, too low
 #or right on the money
+#the player will have a limited number of guesses
+
 
 #imports
 import random
@@ -11,15 +13,17 @@ import random
 #output to explain game to player
 print("\tWelcome to 'Guess My Number'!")
 print("\nI'm thinking of a number between 1 and 100.")
-print("Try to guess it in as few attempts as possible.\n")
+print("Try to guess it within a certain number of guesses.\n")
+
 
 #setting initial values
 the_number = random.randint(1, 100)
+limit = int(input("How many tries would you like? "))
 guess = int(input("Take a guess: "))
 tries = 1
 
 #guessing loop
-while guess != the_number:
+while guess != the_number and tries < limit:
     if guess > the_number:
         print("Lower...")
     else:
@@ -28,7 +32,11 @@ while guess != the_number:
     guess = int(input("Take a guess: "))
     tries += 1
 
-print("You guessed it!  The number was", the_number)
-print("And it only took you", tries, "tries!\n")
+if guess == the_number:
+    print("You guessed it!  The number was", the_number)
+    print("And it only took you", tries, "tries!\n")
+else:
+    print("Sorry, better luck next time")
+    print("The number was", the_number)
 
 input("\n\nPress the enter key to exit.")
